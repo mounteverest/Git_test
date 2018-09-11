fc5971d577c2779062b1dfa1f70b4b27e263817e
kalya <kalya@SKLAPTOP>
2018-09-10 18:05:12 -0700

    A Sample Calculator

---
+++ Git_test/src/com/first/Calculator.java
@@ -0,0 +1,12 @@
+package com.first;
+
+public class Calculator {
+	public int add(int i,int j){
+		return i+j;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
